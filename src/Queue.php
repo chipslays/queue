716ae0ab46748c810ac57bd280ad467de206c115
chipslays <hello@chipslays.ru>
2021-11-17 11:27:01 +0400

    update

--- src/Queue.php
+++ src/Queue.php
@@ -4,9 +4,20 @@ namespace Chipslays\Queue;
 
 use Chipslays\Collection\Collection;
 use Chipslays\Queue\Drivers\DriverInterface;
+use Exception;
 
 define('QUEUE_DEFAULT_SORT', 500);
 
+/**
+ * @method string add(string $channel, array $data, int $sort = QUEUE_DEFAULT_SORT)
+ * @method array|null get(string $channel, string $id)
+ * @method array|null next(string $channel)
+ * @method Collection|null first(string $channel)
+ * @method boolean delete($channel, string $id = null)
+ * @method array|null list(string $channel)
+ * @method int count(string $channel)
+ * @method int position(string $channel, string $id)
+ */
 class Queue
 {
     /**
@@ -22,99 +33,13 @@ class Queue
         $this->driver = $driver;
     }
 
-    /**
-     * Add item to queue.
-     *
-     * Returns the `id` of the added item.
-     *
-     * @param string $channel
-     * @param array $data
-     * @param integer $sort
-     * @return string
-     */
-    public function add(string $channel, array $data, int $sort = QUEUE_DEFAULT_SORT): string
+    public function __call($name, $arguments)
     {
-        return $this->driver->add($channel, $data, $sort);
+        if (!method_exists($this->driver, $name)) {
+            throw new Exception("Method {$name} not exists in " . get_class($this->driver), 1);
+        }
+
+        return call_user_func_array([$this->driver, $name], $arguments);
     }
 
-    /**
-     * Get list of queue items.
-     *
-     * Returns array of `id's`, if `channel` not exists returns `null`.
-     *
-     * @param string $channel
-     * @return array|null
-     */
-    public function list(string $channel = ''): ?array
-    {
-        return $this->driver->list($channel);
-    }
-
-    /**
-     * Get count of items in queue.
-     *
-     * Returns count, if `channel` not exists returns 0.
-     *
-     * @param string $channel
-     * @return integer
-     */
-    public function count(string $channel = ''): int
-    {
-        return $this->driver->count($channel);
-    }
-
-    /**
-     * Get next item in queue.
-     *
-     * Returns array of data, if queue is empty returns `null`.
-     *
-     * @param string $channel
-     * @return array|null
-     */
-    public function next(string $channel = ''): ?array
-    {
-        return $this->driver->next($channel);
-    }
-
-    /**
-     * Get first item in queue.
-     *
-     * Returns `Collection` of array with `channel`, `id`, `data`,
-     * if queue is empty or `channel` not exists returns `null`.
-     *
-     * @param string $channel
-     * @return Collection|null
-     */
-    public function first(string $channel = ''): ?Collection
-    {
-        return $this->driver->first($channel);
-    }
-
-    /**
-     * Delete item from queue.
-     *
-     * Returns `true` on success delete and `false` on fail.
-     *
-     * @param string|Collection $channel Can pass as result from `first` method.
-     * @param string $id
-     * @return boolean
-     */
-    public function delete($channel, string $id = null): bool
-    {
-        return $this->driver->delete($channel, $id);
-    }
-
-    /**
-     * Get item position in queue.
-     *
-     * Return position, if `channel` or `id` not exists returns 0.
-     *
-     * @param string $channel
-     * @param string $id
-     * @return int
-     */
-    public function position(string $channel, string $id): int
-    {
-        return $this->driver->position($channel, $id);
-    }
 }
